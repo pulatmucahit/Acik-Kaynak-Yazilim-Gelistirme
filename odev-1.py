0ace235a16dec4d5e19a9f739248aa187e09adfe
pulatmucahit <116796075+pulatmucahit@users.noreply.github.com>
2023-12-12 22:01:14 +0300

    Update odev-1.py

--- odev-1.py
+++ odev-1.py
@@ -1,62 +1,15 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
-import pandas as pd
+import requests
 
-app = Flask(__name__)
-api = Api(app)
+url = "https://api.coindcx.com/exchange/v1/markets_details"
+response = requests.get(url)
+data = response.json()
 
-class Users(Resource):
-    def get(self):
-        data = pd.read_csv('users.csv')
-        data = data.to_dict('records')
-        return {'data' : data}, 200
+# ETH ile ilgili olanları filtrele
+btc_data = [market for market in data if 'ETH' in market.get('symbol', '')]
 
-    def post(self):
-        json = request.get_json()
-        req_data = pd.DataFrame({
-            'name'      : ['name']],
-            'age'       : ['age']],
-            'city'      : ['city']]
-        })
-        data = pd.read_csv('users.csv')
-        # data = pd.concat([data, req_data], ignore_index=True)
-        data = pd.concat([data, req_data], ignore_index=True)
-        data.to_csv('users.csv', index=False)
-        return {'message' : 'Record successfully added.'}, 201
-
-    def delete(self):
-        name = request.args['name']
-        data = pd.read_csv('users.csv')
-
-        if name in data['name'].values:
-            data = data[data['name'] != name]
-            data.to_csv('users.csv', index=False)
-            return {'message': 'Record successfully deleted.'}, 200
-        else:
-            return {'message': 'Record not found.'}, 404
-
-class Cities(Resource):
-    def get(self):
-        data = pd.read_csv('users.csv',usecols=[2])
-        data = data.to_dict('records')
-        return {'data' : data}, 200
-
-class Name(Resource):
-    def get(self,name):
-        data = pd.read_csv('users.csv')
-        data = data.to_dict('records')
-        for entry in data:
-            if entry['name'] == name :
-                return {'data' : entry}, 200
-        return {'message' : 'No entry found with this name !'}, 404
-
-
-# Add URL endpoints
-api.add_resource(Users, '/users')
-api.add_resource(Cities, '/cities')
-api.add_resource(Name, '/<string:name>')
-
-
-if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=5000)
-    app.run()
+# Filtrelenmiş verilerin min ve max değerlerini ekrana yazdır
+for market in btc_data:
+    print(f"Symbol: {market['symbol']}")
+    print(f"Min Price: {market.get('min_price', 'N/A')}")
+    print(f"Max Price: {market.get('max_price', 'N/A')}")
+    print("---------")
